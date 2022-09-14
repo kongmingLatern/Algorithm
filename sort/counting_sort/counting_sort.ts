ef2c55b858d5d0b63f94f30f67d58f9428d406fa
凤之兮原 <1577310596@qq.com>
2022-09-14 21:52:44 +0800

    feat: add counting_sort.ts

---
+++ sort/counting_sort/counting_sort.ts
@@ -0,0 +1,26 @@
+const countingSortArray = (nums: number[]) => {
+  const len = nums.length
+  if (len < 2) {
+    return nums
+  }
+  const count: number[] = []
+  for (let i = 0; i < len; i++) {
+    const j = nums[i]
+    if (count[j]) {
+      count[j]++
+    } else {
+      count[j] = 1
+    }
+  }
+
+  const res: number[] = []
+  for (let i = 0; i < count.length; i++) {
+    while (count[i] > 0) {
+      res.push(i)
+      count[i]--
+    }
+  }
+  return res
+}
+
+console.log(countingSortArray([5, 1, 3, 9, 2, 4]))
