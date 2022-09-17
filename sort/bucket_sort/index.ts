90c526c084a104cff79e857a6aff2361009f54fa
凤之兮原 <1577310596@qq.com>
2022-09-17 21:09:24 +0800

    chore: add commnets

--- sort/bucket_sort/index.ts
+++ sort/bucket_sort/index.ts
@@ -15,6 +15,8 @@ function sortArrayForBuckjet(nums: number[], size: number) {
   const bucketCount = Math.floor((max - min) / 3) + 1
 
   nums.forEach(item => {
+    // ~~ 就是执行两次按位取反，其实就是保持原值，但是注意虽然是原值，但是对布尔型变量执行这个操作，会转化成相应的数值型变量，也就是
+    // ~~true === 1，~~false === 0。 ~~"" == 0 ~~[] == 0
     const j = ~~((item - min) / size)
     if (buckets[j]) {
       buckets[j].push(item)
