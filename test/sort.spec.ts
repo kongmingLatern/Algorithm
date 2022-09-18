1e3583e02876295d43c3d3802b6118dffd9f5330
凤之兮原 <1577310596@qq.com>
2022-09-18 20:49:23 +0800

    feat: add shellSort test

--- test/sort.spec.ts
+++ test/sort.spec.ts
@@ -4,6 +4,7 @@ import { countingSortArray } from './../sort/counting_sort/index'
 import sortArrayForBucket from '../sort/bucket_sort'
 import heap_sort from '../sort/heap_sort'
 import mergeSortArr from '../sort/mergeSort'
+import shellSort from '../sort/shellSort'
 describe('sort', () => {
   // 堆排序
   it('heap_sort', () => {
@@ -15,16 +16,19 @@ describe('sort', () => {
   })
   // 计数排序
   it('counting_sort', () => {
-    expect(countingSortArray([2, 3, 45, 2, 3, 4, 1], 3)).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
+    expect(countingSortArray([2, 3, 45, 2, 3, 4, 1])).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
   })
 
   // 归并排序
   it('merge_sort', () => {
-    expect(mergeSortArr([2, 3, 45, 2, 3, 4, 1], 3)).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
+    expect(mergeSortArr([2, 3, 45, 2, 3, 4, 1])).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
   })
   // 基数排序
   it('redix_sort', () => {
-    expect(sortArrayForRedix([2, 3, 45, 2, 3, 4, 1], 3)).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
+    expect(sortArrayForRedix([2, 3, 45, 2, 3, 4, 1])).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
   })
   // 希尔排序
+  it('shell_sort', () => {
+    expect(shellSort([2, 3, 45, 2, 3, 4, 1])).toStrictEqual([1, 2, 2, 3, 3, 4, 45])
+  })
 })
