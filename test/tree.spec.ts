a707314febb7c2c0adf0a63a74794637ae208bda
凤之兮原 <1577310596@qq.com>
2023-01-03 10:29:22 +0800

    chore: add test cases about symmetry

--- test/tree.spec.ts
+++ test/tree.spec.ts
@@ -1,4 +1,5 @@
 import { levelOrder } from '../tree/print/bfs'
+import { symmetry } from '../tree/symmetry'
 import { TreeNode } from '../utils'
 
 describe('bfs levelOrder', () => {
@@ -9,5 +10,26 @@ describe('bfs levelOrder', () => {
     root.right.left = new TreeNode(15)
     root.right.right = new TreeNode(7)
     expect(levelOrder(root)).toEqual([3, 9, 20, 15, 7])
+  })
+})
+
+describe('symmetry', () => {
+  it('should not symmetry', () => {
+    const root = new TreeNode(1)
+    root.left = new TreeNode(2)
+    root.right = new TreeNode(2)
+    root.left.left = new TreeNode(3)
+    root.left.right = new TreeNode(4)
+    expect(symmetry(root)).toEqual(false)
+  })
+  it('should symmetry', () => {
+    const root = new TreeNode(1)
+    root.left = new TreeNode(2)
+    root.right = new TreeNode(2)
+    root.left.left = new TreeNode(3)
+    root.left.right = new TreeNode(4)
+    root.right.left = new TreeNode(4)
+    root.right.right = new TreeNode(3)
+    expect(symmetry(root)).toEqual(true)
   })
 })
