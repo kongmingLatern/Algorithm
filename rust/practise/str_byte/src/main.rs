b34f279120ffa0eea1c2bcc350f7303bf562bdd2
凤之兮原 <1577310596@qq.com>
2023-01-07 22:25:37 +0800

    feat: add rust str_byte

---
+++ rust/practise/str_byte/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {}
+#[cfg(test)]
+fn test_str_byte() {
+    let mut bytes = "bors".bytes();
+    assert_eq!(Some(b'b'), bytes.next());
+    assert_eq!(Some(b'o'), bytes.next());
+    assert_eq!(Some(b'r'), bytes.next());
+    assert_eq!(Some(b's'), bytes.next());
+    assert_eq!(None, bytes.next());
+}
