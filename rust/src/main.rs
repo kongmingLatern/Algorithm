39ab6fae92b9520c4596064d172080c6aca240db
凤之兮原 <1577310596@qq.com>
2023-01-09 12:37:52 +0800

    chore: add two_ref about rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,39 @@
+#[warn(dead_code)]
+#[derive(Clone, Debug)]
+struct ListNode {
+    val: i32,
+    next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            val,
+            next: None,
+        }
+    }
+}
+struct Solution {}
+impl Solution {
+    pub fn get_kth_from_end(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let mut slow = &head;
+        let mut fast = &head;
+
+        for _ in 0..k {
+            fast = &fast.as_ref().unwrap().next;
+        }
+
+        while fast.is_some() {
+            fast = &fast.as_ref().unwrap().next;
+            slow = &slow.as_ref().unwrap().next;
+        }
+
+        slow.clone()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("The first ListNode: {:?}", ListNode::new(10));
+    Solution::get_kth_from_end(Some(Box::new(ListNode::new(10))), 10);
 }
